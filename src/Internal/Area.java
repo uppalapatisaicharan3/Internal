aefe4cb6ea5591f2d150e419b17fe7bf0342c077
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:51:13 +0530

    Area and SimpleInterest code is added

--- src/Internal/Area.java
+++ src/Internal/Area.java
@@ -1,10 +1,30 @@
 package Internal;
 
-public class Area {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		class AreaDemo
+		{
+		    void area(int x,int y)
+		    {
+		        System.out.println("the area of the triangle  is "+0.5*x*y+" sq units");
+		    }
+		    void area(float x, float y)
+		    {
+		        System.out.println("the area of the rectangle is "+x*y+" sq units");
+		    }
+		    void area(double x)
+		    {
+		        double z = 3.14 * x * x;
+		        System.out.println("the area of the circle is "+z+" sq units");
+		    }
+		}
+		public class Area
+		{
+		     public static void main(String args[]) 
+			{
+			   AreaDemo ob = new  AreaDemo();
+			   ob.area(5,5);
+			   ob.area(11,12);
+			   ob.area(2.5);
+		        }
 
 	}
 
-}
